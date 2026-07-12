83048aad80257cc633b0246f60c3266951922f15
Aritra Basak <104417333+AritraBasakCode@users.noreply.github.com>
2026-07-12 16:59:50 +0530

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("totalAssets").textContent = data.totalAssets;
-document.getElementById("availableAssets").textContent = data.availableAssets;
-document.getElementById("allocatedAssets").textContent = data.allocatedAssets;
-document.getElementById("dueReturns").textContent = data.dueReturns;
-document.getElementById("maintenanceRequests").textContent = data.maintenanceRequests;
-document.getElementById("utilizationRate").textContent = data.utilizationRate;
-document.getElementById("username").textContent = data.username;
-document.getElementById("userRole").textContent = data.role;
